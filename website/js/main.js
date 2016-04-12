f659496d50d3b80a61c155fd01844d695ffa42ac
Tim Clemans <tim@insideyourgovernment.com>
2016-04-12 10:13:09 -0700

    Update main.js

--- website/js/main.js
+++ website/js/main.js
@@ -12,7 +12,7 @@ function organisation_chart(all_data, selection_string) {
 
     var selection_string = selection_string
 
-    var total_height = 400; //Take height and width from size of svg container
+    var total_height = 1500; //Take height and width from size of svg container
 
     var total_width = $(selection_string).width();
 
